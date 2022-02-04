9e353d125c9f3bae43ac02fa8b4d0fc21885d24b
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-02-04 20:02:57 -0300

    Created theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 
 import GlobalStyles from "./styles/GlobalStyles";
 
+import { ThemeProvider } from "styled-components";
+import dark from "./styles/themes/dark";
+
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme={dark}>
       <GlobalStyles />
       <h1>Reserve.io</h1>
-    </>
+    </ThemeProvider>
   );
 };
 
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,22 @@
+export default {
+  title: "light",
+
+  colors: {
+    // Background
+    primary: "#DCDCDC",
+    secondary: "#FFFFFF",
+    tertiary: "#F5F5F5",
+
+    // Text
+    white: "#000000",
+    black: "#FFFFFF",
+    gray: "#BFBFBF",
+
+    // Elements
+    main: "#4E41F0",
+    success: "#03BB85",
+    info: "#2196F3",
+    warning: "#F7931B",
+    danger: "#E44C4E",
+  },
+};
