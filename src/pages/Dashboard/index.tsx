805714019f97a41143cc2b691b016f0fb8573796
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-02-16 22:32:41 -0300

    Using component ContentHeader

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,11 +3,24 @@ import React from "react";
 import { Container } from "./styles";
 
 import ContentHeader from "components/ContentHeader";
+import SelectInput from "components/SelectInput";
+
+import { useTheme } from "styled-components";
 
 const Dashboard: React.FC = () => {
+  const theme = useTheme();
+
+  const options = [
+    { value: "2021", label: "2021" },
+    { value: "2022", label: "2022" },
+  ];
+
   return (
     <Container>
-      <ContentHeader />
+      <ContentHeader title="Dashboard" lineColor={theme.colors.main}>
+        <SelectInput options={options} />
+        <SelectInput options={options} />
+      </ContentHeader>
     </Container>
   );
 };
