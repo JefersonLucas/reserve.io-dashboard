e41c3227aa22dee1f8acba5472fe9e9a2c55c741
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-04 11:48:05 -0300

    Created mock file

---
+++ src/mock/index.ts
@@ -0,0 +1,127 @@
+export const options = [
+  { value: "2021", label: "2021" },
+  { value: "2022", label: "2022" },
+];
+
+export const finances = [
+  {
+    id: 1,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 2,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 3,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 4,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 5,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 6,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 7,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 8,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 9,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 10,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 11,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 12,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 13,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 14,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 15,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 16,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 17,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 18,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 19,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+  {
+    id: 20,
+    title: "Conta de luz",
+    subtitle: "27/07/2020",
+    amount: "R$ 130,00",
+  },
+];
