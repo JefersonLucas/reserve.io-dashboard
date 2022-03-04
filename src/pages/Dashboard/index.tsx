f4ce066af1e3f4bd342c989c168f941591fadb01
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-04 11:49:20 -0300

    Using HistoryFinance list component and mocked data

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,26 +1,35 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
 
 import ContentHeader from "components/ContentHeader";
 import SelectInput from "components/SelectInput";
+import HistoryFinanceList from "components/HistoryFinanceList";
+
+import { options, finances } from "mock";
 
 import { useTheme } from "styled-components";
 
 const Dashboard: React.FC = () => {
   const theme = useTheme();
-
-  const options = [
-    { value: "2021", label: "2021" },
-    { value: "2022", label: "2022" },
-  ];
-
   return (
     <Container>
       <ContentHeader title="Dashboard" lineColor={theme.colors.main}>
         <SelectInput options={options} />
         <SelectInput options={options} />
       </ContentHeader>
+      <Content>
+        {finances.map(({ id, title, subtitle, amount }) => (
+          <HistoryFinanceList
+            key={id}
+            cardColor={theme.colors.secondary}
+            tagColor={theme.colors.main}
+            title={title}
+            subtitle={subtitle}
+            amount={amount}
+          />
+        ))}
+      </Content>
     </Container>
   );
 };
