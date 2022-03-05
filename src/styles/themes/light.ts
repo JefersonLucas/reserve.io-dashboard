f2c443cca7fb08039e1e9371f42e86ce0b25ab9f
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-05 19:12:13 -0300

    Added new items to the theme

--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -3,28 +3,48 @@ export default {
   title: "light",
 
   colors: {
-    // Background
-    primary: "#DCDCDC",
-    secondary: "#FFFFFF",
-    tertiary: "#F5F5F5",
-
-    // Text
-    white: "#000000",
-    black: "#FFFFFF",
-    gray: "#BFBFBF",
-
-    // Elements
     main: "#4E41F0",
-    success: "#03BB85",
-    info: "#2196F3",
-    warning: "#FFB74D",
-    danger: "#E44C4E",
 
-    // Shadows
-    shadows: {
+    background: {
+      primary: "#DCDCDC",
+      secondary: "#FFFFFF",
+      tertiary: "#F5F5F5",
+    },
+
+    text: {
+      white: "#000000",
+      black: "#FFFFFF",
+      gray: "#BFBFBF",
+    },
+
+    status: {
+      success: "#03BB85",
+      info: "#2196F3",
+      warning: "#FFB74D",
+      danger: "#E44C4E",
+    },
+  },
+  shadows: {
+    colors: {
       light: "rgba(0, 0, 0, 0.2)",
       medium: "rgba(0, 0, 0, 0.5)",
       dark: "rgba(0, 0, 0, 0.8)",
+    },
+
+    elevation: {
+      sm: "0 0 0.7em",
+      md: "1px 1px 2px 2px",
+      lg: "1px 1px 5px 5px",
+    },
+  },
+  animation: {
+    duration: {
+      instant: "0s",
+      faster: "0.1s",
+      fast: "0.2s",
+      normal: "0.3s",
+      slow: "0.4s",
+      slower: "0.5s",
     },
   },
 };
