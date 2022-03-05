d856733819e96e3d2973b4b46a5eaef52ee4b649
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-04 22:43:37 -0300

    Moved components to reservations page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,37 +1,8 @@
 import React from "react";
-
-import { Container, Content } from "./styles";
-
-import ContentHeader from "components/ContentHeader";
-import SelectInput from "components/SelectInput";
-import HistoryFinanceList from "components/HistoryFinanceList";
-
-import { options, finances } from "mock";
-
-import { useTheme } from "styled-components";
+import { Container } from "components/Layout/styles";
 
 const Dashboard: React.FC = () => {
-  const theme = useTheme();
-  return (
-    <Container>
-      <ContentHeader title="Dashboard" lineColor={theme.colors.main}>
-        <SelectInput options={options} />
-        <SelectInput options={options} />
-      </ContentHeader>
-      <Content>
-        {finances.map(({ id, title, subtitle, amount }) => (
-          <HistoryFinanceList
-            key={id}
-            cardColor={theme.colors.secondary}
-            tagColor={theme.colors.main}
-            title={title}
-            subtitle={subtitle}
-            amount={amount}
-          />
-        ))}
-      </Content>
-    </Container>
-  );
+  return <Container>Dashboard</Container>;
 };
 
 export default Dashboard;
