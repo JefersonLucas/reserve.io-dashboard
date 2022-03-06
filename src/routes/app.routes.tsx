f907c175bfd684e8aca1a8da604db78bf1d321ab
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-06 00:55:37 -0300

    Created application routes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "components/Layout";
+
+import Dashboard from "pages/Dashboard";
+import Register from "pages/Register";
+import Reservations from "pages/Reservations";
+import Error from "pages/Error";
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} caseSensitive />
+        <Route path="/register" element={<Register />} caseSensitive />
+        <Route path="/reservations" element={<Reservations />} caseSensitive />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Layout>
+  );
+};
+
+export default AppRoutes;
