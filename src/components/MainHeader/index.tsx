f9b3281e7e2254884ce6ca75ffa177f181b5fa94
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-07 16:41:16 -0300

    Using hook useThemeContext and passing to toogle component

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,12 +1,25 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 
 import { Container, Profile, Welcome, UserName } from "./styles";
 
-import Toggle from "../Toggle";
+import Toggle from "components/Toggle";
 
 import emojis from "utils/emojis";
 
+import useThemeContext from "hooks/useThemeContext";
+
 const MainHeader: React.FC = () => {
+  const { toggleTheme, theme } = useThemeContext();
+
+  const [isTheme, setIsTheme] = useState(() =>
+    theme.title === "dark" ? true : false,
+  );
+
+  const handleChangeTheme = () => {
+    setIsTheme(!isTheme);
+    toggleTheme();
+  };
+
   const emoji = useMemo(() => {
     const index = Math.floor(Math.random() * emojis.length);
     return emojis[index];
@@ -14,7 +27,7 @@ const MainHeader: React.FC = () => {
 
   return (
     <Container>
-      <Toggle />
+      <Toggle checked={isTheme} onChange={handleChangeTheme} />
       <Profile>
         <Welcome>Olá, {emoji}</Welcome>
         <UserName>Jeferson Lucas</UserName>
