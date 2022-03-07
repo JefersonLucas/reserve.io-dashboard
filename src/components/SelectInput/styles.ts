8fe6e4444d6a5c522db7ff4524c8ec1dd85aeb9f
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-07 16:49:31 -0300

    Added style for focus

--- src/components/SelectInput/styles.ts
+++ src/components/SelectInput/styles.ts
@@ -7,11 +7,19 @@ export const Select = styled.select`
 
   border-radius: 5px;
   color: ${({ theme }) => theme.colors.text.white};
+  border: 1px solid transparent;
+
   background-color: ${({ theme }) => theme.colors.background.tertiary};
   box-shadow: ${({ theme }) => theme.shadows.elevation.md}
     ${({ theme }) => theme.shadows.colors.light};
+  transition: 0.3s;
 
   padding: 7px 10px;
+
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.colors.main};
+    box-shadow: 0 0 2px 1px ${({ theme }) => theme.colors.main};
+  }
 `;
 
 export const Option = styled.option``;
