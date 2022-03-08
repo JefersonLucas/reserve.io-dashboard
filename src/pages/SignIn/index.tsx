a02c7538580d9bd501b26043ccb8abc39576f9cc
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-08 17:56:19 -0300

    Passing state to authentication and corrected password type

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Input from "components/Input";
 import Button from "components/Button";
@@ -7,7 +7,12 @@ import Head from "components/Head";
 import * as S from "./styles";
 import logo from "assets/logo.svg";
 
+import useAuthContext from "hooks/useAuthContext";
+
 const SignIn: React.FC = () => {
+  const { signIn } = useAuthContext();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   return (
     <>
       <Head title="Entrar" description="Entre com a sua conta" />
@@ -16,11 +21,21 @@ const SignIn: React.FC = () => {
           <S.Logo src={logo} alt="Logo Reserve" />
           <S.Title>Reserve.io</S.Title>
         </S.Header>
-        <S.Form>
+        <S.Form onSubmit={() => signIn(email, password)}>
           <S.FormTitle>Entrar</S.FormTitle>
           <S.InputContainer>
-            <Input type="email" placeholder="E-mail" required />
-            <Input type="senha" placeholder="Senha" required />
+            <Input
+              type="email"
+              placeholder="E-mail"
+              onChange={({ target }) => setEmail(target.value)}
+              required
+            />
+            <Input
+              type="password"
+              placeholder="Senha"
+              required
+              onChange={({ target }) => setPassword(target.value)}
+            />
           </S.InputContainer>
           <Button type="submit">Acessar</Button>
         </S.Form>
