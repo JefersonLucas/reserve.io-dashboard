93cc3b32b22f409365f9e6088f26e1a66ac93a24
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-17 17:30:20 -0300

    Added API call

--- src/pages/Reservations/index.tsx
+++ src/pages/Reservations/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { Container, Content } from "./styles";
 
 import ContentHeader from "components/ContentHeader";
@@ -5,13 +6,45 @@ import SelectInput from "components/SelectInput";
 import ReservationList from "components/ReservationList";
 import Filters from "components/Filters";
 import Head from "components/Head";
+import Loader from "components/Loader";
+import Error from "components/Error";
 
-import { months, years, reservations, controls } from "mocks";
+import { months, years, controls } from "mocks";
+
+import { GET_RESERVATION } from "api";
+
+import { Reservation } from "@types";
+
+import formatDate from "helpers/formatDate";
 
 import { useTheme } from "styled-components";
 
+import useFetch from "hooks/useFetch";
+import useLocalStorage from "hooks/useLocalStorage";
+import useAuthContext from "hooks/useAuthContext";
+
 const Reservations: React.FC = () => {
   const theme = useTheme();
+
+  const { logout } = useAuthContext();
+
+  const token = useLocalStorage.get("token");
+
+  if (!token) {
+    logout();
+    return null;
+  }
+
+  const { url, options } = GET_RESERVATION(token);
+
+  const {
+    data: reservations,
+    loading,
+    error,
+  } = useFetch<Reservation[]>(url, options);
+
+  if (error) return <Error error={error as unknown as string} />;
+  if (loading) return <Loader />;
   return (
     <>
       <Head title="Reservas" description="Visulize as reservas efetuadas" />
@@ -24,8 +57,12 @@ const Reservations: React.FC = () => {
         <Filters controls={controls} />
 
         <Content>
-          {reservations.map((items) => (
-            <ReservationList key={items.id} {...items} />
+          {reservations?.map((items) => (
+            <ReservationList
+              {...items}
+              date={formatDate(items.date)}
+              key={items.id}
+            />
           ))}
         </Content>
       </Container>
