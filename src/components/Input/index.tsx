906fd007f84945970e1da81b2ae30ddea68fad0e
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-16 23:12:21 -0300

    Updated input component with value, onBlur, onChange and error.

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { Container, Error } from "./styles";
 
 import { InputProps } from "./types";
 
-const Input: React.FC<InputProps> = ({ ...props }) => {
-  return <Container {...props} />;
-};
+const Input: React.FC<InputProps> = ({ ...props }) => (
+  <>
+    <Container
+      {...props}
+      onChange={props.onChange}
+      value={props.value}
+      onBlur={props.onBlur}
+    />
+    {props.error ? <Error>{props.error}</Error> : <Error> </Error>}
+  </>
+);
 
 export default Input;
