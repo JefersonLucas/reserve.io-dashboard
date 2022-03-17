aa5e8e2a12427b7cca146a2661f0ea556713e5aa
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-17 17:26:53 -0300

    Extended ReservationList from Reservation interface

--- src/components/ReservationList/types.d.ts
+++ src/components/ReservationList/types.d.ts
@@ -1,14 +1,6 @@
-import { Status } from "@types";
+import { Status, Reservation } from "@types";
 
-export interface ReservationListProps {
-  id: string;
-  status: Status;
-  requester: string;
-  date: string;
-  place: string;
-  entry_time: string;
-  exit_time: string;
-}
+export interface ReservationListProps extends Reservation {}
 
 export interface TagProps {
   status: Status;
