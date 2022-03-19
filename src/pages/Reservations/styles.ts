3730b10df25578f5438146bceb95fe5f1e913122
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-18 23:48:44 -0300

    Created NotContent style

--- src/pages/Reservations/styles.ts
+++ src/pages/Reservations/styles.ts
@@ -2,3 +2,12 @@ import styled from "styled-components";
 
 export const Container = styled.div``;
 export const Content = styled.div``;
+export const NotContent = styled.div`
+  display: flex;
+  align-content: center;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 200px;
+`;
