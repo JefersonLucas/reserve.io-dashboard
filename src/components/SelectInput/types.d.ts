d09b390ebf4384685731d69ac0320eefdf2417c4
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-18 23:47:43 -0300

    Added onChange and defaultValue props

--- src/components/SelectInput/types.d.ts
+++ src/components/SelectInput/types.d.ts
@@ -1,6 +1,10 @@
+import React from "react";
+
 export interface SelectInputProps {
   options: {
     value: string | number;
     label: string | number;
   }[];
+  onChange?(event: React.ChangeEvent<HTMLSelectElement>): void | undefined;
+  defaultValue?: string | number;
 }
