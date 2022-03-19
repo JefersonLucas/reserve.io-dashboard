2a6731482453520e319e7e01432259c6afa568f3
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-18 23:50:09 -0300

    Added filter by month and year

--- src/pages/Reservations/index.tsx
+++ src/pages/Reservations/index.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { Container, Content } from "./styles";
+import { useTheme } from "styled-components";
+
+import { Container, Content, NotContent } from "./styles";
 
 import ContentHeader from "components/ContentHeader";
 import SelectInput from "components/SelectInput";
@@ -15,13 +17,12 @@ import { GET_RESERVATION } from "api";
 
 import { Reservation } from "@types";
 
-import formatDate from "helpers/formatDate";
-
-import { useTheme } from "styled-components";
-
 import useFetch from "hooks/useFetch";
 import useLocalStorage from "hooks/useLocalStorage";
 import useAuthContext from "hooks/useAuthContext";
+import useReservationsFiltered from "hooks/useReservationsFiltered";
+
+import returnMonth from "helpers/returnMonth";
 
 const Reservations: React.FC = () => {
   const theme = useTheme();
@@ -32,16 +33,21 @@ const Reservations: React.FC = () => {
 
   if (!token) {
     logout();
+    window.location.href = "/";
     return null;
   }
 
   const { url, options } = GET_RESERVATION(token);
 
+  const { data, loading, error } = useFetch<Reservation[]>(url, options);
+
   const {
-    data: reservations,
-    loading,
-    error,
-  } = useFetch<Reservation[]>(url, options);
+    reservations,
+    yearSeleted,
+    setYearSelected,
+    monthSeleted,
+    setMonthSelected,
+  } = useReservationsFiltered(data);
 
   if (error) return <Error error={error as unknown as string} />;
   if (loading) return <Loader />;
@@ -50,21 +56,37 @@ const Reservations: React.FC = () => {
       <Head title="Reservas" description="Visulize as reservas efetuadas" />
       <Container>
         <ContentHeader title="Reservas" lineColor={theme.colors.main}>
-          <SelectInput options={months} />
-          <SelectInput options={years} />
+          <SelectInput
+            options={months}
+            onChange={({ target }) => setMonthSelected(target.value)}
+            defaultValue={monthSeleted}
+          />
+          <SelectInput
+            options={years}
+            onChange={({ target }) => setYearSelected(target.value)}
+            defaultValue={yearSeleted}
+          />
         </ContentHeader>
 
-        <Filters controls={controls} />
+        {reservations && reservations.length > 0 ? (
+          <Content>
+            <Filters controls={controls} />
 
-        <Content>
-          {reservations?.map((items) => (
-            <ReservationList
-              {...items}
-              date={formatDate(items.date)}
-              key={items.id}
-            />
-          ))}
-        </Content>
+            {reservations.map((items) => (
+              <ReservationList {...items} key={items.id} />
+            ))}
+          </Content>
+        ) : (
+          <NotContent>
+            <p>
+              Não há <strong>reservas</strong> no período de{" "}
+              <strong>
+                {returnMonth(monthSeleted)} de {yearSeleted}
+              </strong>
+              .
+            </p>
+          </NotContent>
+        )}
       </Container>
     </>
   );
